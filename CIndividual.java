5e37315b2cb46628afbc0914da50c6fa997722eb
vicalbiter <vic.albiter@gmail.com>
2019-03-21 01:03:09 -0600

    Test CIndividual class and make a proof of concept for the Max Priority Queue

--- CIndividual.java
+++ CIndividual.java
@@ -1,4 +1,6 @@
 import java.lang.Comparable;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class CIndividual implements Comparable<CIndividual> {
     
@@ -16,5 +18,24 @@ public class CIndividual implements Comparable<CIndividual> {
         if (this.fitness < that.fitness) { return -1; }
         else if (this.fitness == that.fitness) { return 0; }
         else { return 1; }
+    }
+    
+    // Test client
+    public static void main(String[] args) {
+        GAUtils utils = new GAUtils();
+        Population population = utils.generatePopulation(70, 64);
+        PriorityQueue<CIndividual> maxpq = new PriorityQueue<CIndividual>(population.length(), Collections.reverseOrder());
+        for (int i = 0; i < population.length(); i++) {
+            Individual ind = population.getIndividualAtIndex(i);
+            double fitness = utils.getFitnessOfIndividual(ind);
+            System.out.println(ind + " Fitness:" + fitness);
+            maxpq.add(new CIndividual(ind, fitness));
+        }
+        System.out.println("PQ Test:");
+        for (int i = 0; i < maxpq.size(); i++) {
+            CIndividual current_max = maxpq.poll();
+            double current_max_fitness = utils.getFitnessOfIndividual(current_max.ind);
+            System.out.println(current_max.ind + " Fitness:" + current_max_fitness);
+        }
     }
 }
