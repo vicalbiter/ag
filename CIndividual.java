a1a3fc311ca4da05f081a532a941ec9a97fa8b7b
vicalbiter <vic.albiter@gmail.com>
2019-03-21 00:43:11 -0600

    Add comments to Cindividual

--- CIndividual.java
+++ CIndividual.java
@@ -10,8 +10,10 @@ public class CIndividual implements Comparable<CIndividual> {
         this.fitness = fitness;
     }
     
+    // Override the compareTo method (i.e. to use the comparable interface, thus allowing us to use a Priority Queue
+    // to sort the Individuals by fitness)
     public int compareTo(CIndividual that) {
-        if (this.fitness < that.fitness) { return - 1; }
+        if (this.fitness < that.fitness) { return -1; }
         else if (this.fitness == that.fitness) { return 0; }
         else { return 1; }
     }
